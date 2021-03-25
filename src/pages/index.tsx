8b2c167aa96fa6e13776abb34bfc86aff0161c26
Francyleo Matos <programadorleomatos@gmail.com>
2021-03-25 04:14:12 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Hero: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Francyleo Matos - Portfolio</title>
+      </Head>
+      <main>
+        <h1>Hello next app</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Hero;
